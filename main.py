7b24854555bfebfcc3a6b42455ba5aeaace0d792
Max Huzmiev <max@huzm.ru>
2022-07-07 10:44:11 +0300

    Bootstrap added and try static files

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
+
 app = FastAPI()
+app.mount('/static', StaticFiles(directory='static'), name='static')
+templates = Jinja2Templates(directory='templates')
 
 @app.get('/')
-def index():
-    return {"Hello": "World"}
+def index(req: Request):
+    return templates.TemplateResponse('base.jinja2', {'request': req})
